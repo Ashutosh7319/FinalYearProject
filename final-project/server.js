169bc80f0d1d2a79fd7346feb9d8413012a8ab21
Priyanshu <sahaashutosh108@gmail.com>
2025-11-24 22:11:47 +0530

    Update server.js

--- final-project/server.js
+++ final-project/server.js
@@ -13,6 +13,7 @@ app.use(
       "http://127.0.0.1:5500",
       "http://localhost:5500",
       "https://teamflex.netlify.app",
+      "https://theutlron.onrender.com/",
     ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
