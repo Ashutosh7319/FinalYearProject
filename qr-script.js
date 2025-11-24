b27006428f536209d831fe14f61d9521b72bf781
Priyanshu <sahaashutosh108@gmail.com>
2025-11-24 10:25:59 +0530

    Update qr-script.js

--- qr-script.js
+++ qr-script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 
   try {
-    const res = await fetch("http://localhost:3000/generate-qr", {
+    const res = await fetch("https://finalyearproject-52g2.onrender.com/generate-qr", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
