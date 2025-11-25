a356fbbccca45df03835032d7a7a70ddf310c8a2
Priyanshu <sahaashutosh108@gmail.com>
2025-11-25 08:43:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "https://teamflex.netlify.app",
-      "https://theutlron.onrender.com",
+      "https://theutrlon.onrender.com",
     ],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type"],
